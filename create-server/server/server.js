48ca0471c1d1ae189710fba3bca138c8179b30b5
dev-hemul <dev.hemulll@gmail.com>
2024-07-17 13:37:54 +0300

    change path

--- create-server/server/server.js
+++ create-server/server/server.js
@@ -8,22 +8,22 @@ const config = {
 const server = http.createServer(async (req, res) => {
 	const {url} = req;
 	if (url === '/') {
-		const index = await fs.readFile('./client/index.html', 'utf8');
+		const index = await fs.readFile('client/index.html', 'utf8');
 		res.statusCode = 200;
 		res.setHeader('Content-Type', 'text/html; charset=utf-8');
 		res.end(index);
-	} else if (url === '/client/style.css') {
-			const css = await fs.readFile('./client/style.css', 'utf8');
+	} else if (url === '/style.css') {
+			const css = await fs.readFile('client/style.css', 'utf8');
 			res.statusCode = 200;
 			res.setHeader('Content-Type', 'text/css; charset=utf-8');
 			res.end(css);
-	}else if (url === '/client/cat.jpg') {
-		const css = await fs.readFile('./client/cat.jpg');
+	}else if (url === '/cat.jpg') {
+		const css = await fs.readFile('client/cat.jpg');
 		res.statusCode = 200;
 		res.setHeader('Content-Type', 'image/png');
 		res.end(css);
 	}	else {
-		const page404 = await fs.readFile('./client/page404.html', 'utf8');
+		const page404 = await fs.readFile('client/page404.html', 'utf8');
 		res.statusCode = 200;
 		res.setHeader('Content-Type', 'text/html; charset=utf-8');
 		res.end(page404);
