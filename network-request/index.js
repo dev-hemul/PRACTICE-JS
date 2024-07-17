822a73e1d5ea68f7c2a1d3ffa5c9c1c1bfe9afec
dev-hemul <dev.hemulll@gmail.com>
2024-07-17 21:22:03 +0300

    modified data

--- network-request/index.js
+++ network-request/index.js
@@ -1,6 +1,6 @@
 // Приклад роботи з fetch
 
-fetch('https://jsonplaceholder.typicode.com/comments'
+fetch('https://jsonplaceholder.typicode.com/users/11'
 ).then((data) => {
 	return data.json()
 }).then((info) => {
