49790e5b716e69a2390c3277b36903264d9984f7
dev-hemul <dev.hemulll@gmail.com>
2024-07-17 11:59:08 +0300

    create new folders

--- create-server/server/server.js
+++ create-server/server/server.js
@@ -8,17 +8,22 @@ const config = {
 const server = http.createServer(async (req, res) => {
 	const {url} = req;
 	if (url === '/') {
-		const index = await fs.readFile('./index.html', 'utf8');
+		const index = await fs.readFile('./client/index.html', 'utf8');
 		res.statusCode = 200;
 		res.setHeader('Content-Type', 'text/html; charset=utf-8');
 		res.end(index);
-	} else if (url === '/style.css') {
-			const css = await fs.readFile('./style.css', 'utf8');
+	} else if (url === '/client/style.css') {
+			const css = await fs.readFile('./client/style.css', 'utf8');
 			res.statusCode = 200;
 			res.setHeader('Content-Type', 'text/css; charset=utf-8');
 			res.end(css);
-	} else {
-		const page404 = await fs.readFile('./page404.html', 'utf8');
+	}else if (url === '/client/cat.jpg') {
+		const css = await fs.readFile('./client/cat.jpg');
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'image/png');
+		res.end(css);
+	}	else {
+		const page404 = await fs.readFile('./client/page404.html', 'utf8');
 		res.statusCode = 200;
 		res.setHeader('Content-Type', 'text/html; charset=utf-8');
 		res.end(page404);
